8b72925d49d05d6da013852d4d00e3bd77772c21
gohdong <aldehf420@naver.com>
2021-07-11 13:47:21 +0900

    Initial Commit

---
+++ CarrotMarketClone/CarrotMarketCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarrotMarketCloneApp.swift
+//  CarrotMarketClone
+//
+//  Created by 고동현 on 2021/07/11.
+//
+
+import SwiftUI
+
+@main
+struct CarrotMarketCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
